d80de1324abaa6144a61c1a93f01c4b367df74d0
Tom <rysertom@gmail.com>
2020-06-03 06:43:59 +0200

    Removed all unused function

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -13,36 +13,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Event;
 
+// TODO: make the event for clubs
 class EventController extends Controller
 {
-    public function index()
-    {
-        return Event::all();
-    }
- 
-    public function show($event)
-    {
-        return Event::find($event);
-    }
-
-    public function store(Request $request)
-    {
-        $event = Event::create($request->all());
-
-        return response()->json($event, 201);
-    }
-
-    public function update(Request $request, Event $event)
-    {
-        $event->update($request->all());
-
-        return response()->json($event, 200);
-    }
-
-    public function delete(Event $event)
-    {
-        $event->delete();
-
-        return response()->json(null, 204);
-    }
 }
