1477b26cada1911b83b558954f5834ea4d4e5688
Tom <rysertom@gmail.com>
2020-06-03 06:47:15 +0200

    updated route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//TODO: all clubs
 Route::get('/', function () {
     return view('/home');
 });
@@ -38,7 +38,7 @@ Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
 Route::get('/user/update', 'UserController@showUpdate');
 Route::get('/user/{username}', 'UserController@show')->name('showUser');;
 
-Route::get('/club/{clubId}', 'ClubController@show');
+Route::get('/club/{clubId}', 'ClubController@show');//TODO: check if club exist
 Route::get('/clubs/all', 'ClubController@index');
 Route::get('/clubs/my', 'ClubController@userClubs');
 Route::get('/clubs/update/{clubName}', 'ClubController@showUpdate')->name('showClub');
@@ -49,22 +49,22 @@ Route::get('/dives/public/{site}', 'DiveController@publicSite')->name('showSite'
 Route::get('/dives/personnal', 'DiveController@personnal');
 Route::get('/dives/personnal/{site}', 'DiveController@personnalSite');
 Route::get('/dives/followed', 'DiveController@followed');
+Route::get('/dive/{diveId}', 'DiveController@show')->name('showDive');
+Route::get('/dive/update/{diveId}', 'DiveController@showUpdate')->name('showUpdateDive');
 
 Route::get('/diveSites/public', 'DiveSiteController@public');
 Route::get('/diveSites/personnal', 'DiveSiteController@personnal');
 Route::get('/diveSites/followed', 'DiveSiteController@followed');
 
-Route::get('/dive/{diveId}', 'DiveController@show')->name('showDive');
-Route::get('/dive/update/{diveId}', 'DiveController@showUpdate')->name('showUpdateDive');
-
 Route::post('/follow', 'UserController@follow');
 Route::post('/uploadDive', 'DiveController@store');
-Route::post('/membership', 'ClubController@member');
+Route::post('/membership', 'ClubController@changeMembership');
 Route::post('/sendUpdateDive', 'DiveController@update');
 Route::post('/sendUpdateClub', 'ClubController@update');
 Route::post('/sendUpdateUser', 'UserController@update');
 Route::post('/sendUpdateSite', 'DiveSiteController@update');
 
+//TODO: change name in form
 Route::post('/Site/update', function () {
 
     $dataUpdate["longitude"] = $_POST["lng"];
